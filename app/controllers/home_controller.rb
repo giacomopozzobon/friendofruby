9c06ed339fe99a58d8487c01ebd073595b5c6f55
Giacomo Pozzobon <g97.pozzobon@gmail.com>
2022-07-07 05:03:54 +0200

    Other Friendly changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
 
   def about
     if user_signed_in? 
-      @about_me = "My name is Giacomo Pozzobon..."
+      @about_me = "I am nothing more than you, "+current_user.first_name
     else
-      @about_me = "Yo, you are not logged. Login to test me."
+      @about_me = "Yo, you are not logged. Login, please."
     end
 
     about = "This variable works only local"
